95ed3e69393728030e7a9a9eeb1659b1eeb24d14
Alyoshka <alyoshkamyshko@gmail.com>
2022-04-22 17:00:47 -0700

    Set up Hangman class variables, and word guess loop

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -4,6 +4,9 @@ class Hangman
 
     def initialize
         @word = generate_word()
+        @correct_letters = set_dashes(@word)
+        @wrong_letters = []
+        @guesse_left = 10
         p @word
     end
 
@@ -15,6 +18,38 @@ class Hangman
             return word unless word.length < 5 || word.length > 12
         end
     end
+
+    def set_dashes(word)
+        dashes = []
+        word.length.times {dashes.push("_")}
+        dashes
+    end
+
+    public
+    def display
+        puts @correct_letters.join(" ")
+        puts "incorrect letters: #{@wrong_letters.join(", ")}"
+        puts "guesses left: #{@guesse_left}"
+    end
+end
+
+def get_guess
+    while true do
+        print "Enter guess: "
+        guess = gets.chomp
+        if guess.match?(/[[:alpha:]]/) && guess.length == 1
+            return guess
+        else
+            puts "Invalid guess. Must input one letter"
+        end
+    end
 end
 
 game = Hangman.new
+while true do # game loop, break when game over
+    game.display
+    guess = get_guess()
+        
+    puts guess
+    break
+end
